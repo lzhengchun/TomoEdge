281b28c96fa0921abb731908697b3e5720cebd75
Zhengchun Liu <liuzhengchun@gmail.com>
2019-10-22 17:47:08 -0500

    adding fbp files

--- test-fbp.sh
+++ test-fbp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 rm fbp.out
-nvcc test_fbp.cpp fbp.cu -lcufft -o fbp.out
+nvcc test_fbp.cpp fbp.cu -lcufft -o fbp.out -std=c++11
 ./fbp.out
--- test-gridrec.sh
+++ test-gridrec.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 rm gridrec.out
-nvcc test_gridrec.cpp gridrec.cu -lcufft -o gridrec.out
+nvcc test_gridrec.cpp gridrec.cu -lcufft -o gridrec.out -std=c++11
 ./gridrec.out
